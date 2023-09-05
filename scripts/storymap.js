ee74948854d4684763a1057fe6cf277715321699
gbrow <guguisbrow@gmail.com>
2023-09-04 21:31:01 -0300

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -353,13 +353,13 @@ $(window).on('load', function() {
 
             function createCustomIcon (feature, latlng) {
               let myIcon = L.icon({
-                iconUrl: 'my-icon.png',
-                shadowUrl: 'my-icon.png',
+                iconUrl: c['Marker Image'],
+                //shadowUrl: c['Marker Image'],
                 iconSize:     [25, 25], // width and height of the image in pixels
-                shadowSize:   [35, 20], // width, height of optional shadow image
-                iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
-                shadowAnchor: [12, 6],  // anchor point of the shadow. should be offset
-                popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+                //shadowSize:   [35, 20], // width, height of optional shadow image
+                iconAnchor:   [12, 12] //, // point of the icon which will correspond to marker's location
+                //shadowAnchor: [12, 6],  // anchor point of the shadow. should be offset
+                //popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
               })
               return L.marker(latlng, { icon: myIcon })
             }
